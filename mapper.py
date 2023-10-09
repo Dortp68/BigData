f66abaf842eca136ab50735072b456047528a0ff
Михаил <53114070+Dortp68@users.noreply.github.com>
2023-10-09 21:39:34 +0300

    mapper.py

--- mapper.py
+++ mapper.py
@@ -2,24 +2,20 @@
 
 import sys
 
-
-def Map(prices) -> str:
-    l = len(prices)
-    m = sum(prices)/l
-    s=0
-    for i in prices:
-        s=s+(i-m)*(i-m)
-    return f'{l}\t{m}\t{s/(l-1)}'
-
-
 def main():
     prices = []
-    line = sys.stdin.readline().strip().split(",")[9]
+    line = sys.stdin.readline().strip()
     while line:
-        prices += [int(line)]
-        line = sys.stdin.readline().strip().split(",")[9]
-	
-    print(Map(prices))
+        s+=1
+	prices += [int(line)]
+        line = sys.stdin.readline().strip()
+    s = len(prices)
+    m = sum(prices)/s
+    v = 0
+    for i in prices:
+	v+=(i-m)**2
+    v = v/(s-1)
+    print(s, m, v)
 
 
 if __name__ == "__main__":
